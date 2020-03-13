27f8065ba6082bb7d644c7719e696db703a9e1f1
Ahmed <ahmad.rizgar.rashid@gmail.com>
2020-03-14 00:45:50 +0100

    request param

--- todo-list/src/main/java/academy/learnprogramming/todolist/TodoListApplication.java
+++ todo-list/src/main/java/academy/learnprogramming/todolist/TodoListApplication.java
@@ -5,14 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /*
 
-lets discuss what our model is as well as the @ModelAttribute annotation firstly though
-let's discuss what the model interfaces, so the model interface defines a holder
-for model attributes and is as such primarily designed for adding attributes
-to the model now that model is exposed o the view which means that we can
-access the attributes in our html
-
-
-
 
 
  */
--- todo-list/src/main/java/academy/learnprogramming/todolist/controllers/IndexController.java
+++ todo-list/src/main/java/academy/learnprogramming/todolist/controllers/IndexController.java
@@ -1,13 +1,27 @@
 package academy.learnprogramming.todolist.controllers;
-
 import academy.learnprogramming.todolist.service.DemoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+/*
+So when we access the welcome page it always displayed the message hallo Ahmed although but the idea now is to display a message
+based on a request parameter the request parameter is actually a query parameter or form data although in this case.
+To get the query parameter we can use the Requestparam annotation.
+So let's add a parameter to our method. the parameter will be annotated with requestparam the parameter type will be string
+and the name will be user we can then use this parameter to get hello message from the service in other words we can
+send this parameter the value of it to hat method.
+
+
+so when we use the request parameter anotation a parameter is required otherwise
+we'll get errors. So the request program annotation has some properties such as
+required which we can use to mark a parameter as optional because the default is required now the name of the
+request parameter will be taken from the parameter name which in our case we've defined on line 39 as user now if you
+ */
 
 @Controller
 public class IndexController {
@@ -22,9 +36,10 @@ public class IndexController {
 
     //== Request methods ==
     @GetMapping("/hello")
-    public String index(Model model){
+    public String index(@RequestParam String user, @RequestParam int age ,Model model){
 
-        model.addAttribute("helloMessage",demoService.getHelloMessage("Ahmed"));
+        model.addAttribute("helloMessage",demoService.getHelloMessage(user));
+        model.addAttribute("age",age);
         return "index";
     }
     // == Model Attributes ==
