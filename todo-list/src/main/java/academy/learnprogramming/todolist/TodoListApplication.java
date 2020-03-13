2267ac4493ec3a05b6531ea15cde1b073bcde66a
Ahmed <ahmad.rizgar.rashid@gmail.com>
2020-03-13 23:10:47 +0100

    Update TodoListApplication.java

--- todo-list/src/main/java/academy/learnprogramming/todolist/TodoListApplication.java
+++ todo-list/src/main/java/academy/learnprogramming/todolist/TodoListApplication.java
@@ -5,25 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /*
 
-As mentioned earlier the spring MVC, Model View controller, is designed around the dispatcher servlet which plays the
-role of a front controller. So in other words it receives all the requests for your application. We can map requests to
-methods in classes annotated with the @controller annotation then these classes
-are known unsurprisingly has annotated controllers or controller classes now
-the spring MVC provides an annotation based programming model where the controller and rest controller
-components use annotations to express things like request mappings request
-import exception handling and more
-
-The @RequestMapping annotation is used map requests to controller methods.
-It has various attributes to match by URL, HTTP protocol, request parameters, headers
-and media types what we'll do is use specific shortcut variants of the
-request mapping annotation.
-
-Shortcut variants of @RequestMapping are:
-- @GetMapping(For mapping HTTP get Requests)
-- @PostMapping(For mapping HTTP post Requests)
-- @PutMapping(For mapping HTTP put Requests)
-- @DeleteMapping(For mapping HTTP delete Requests)
-- @PatchMapping(For mapping HTTP patch Requests)
+lets discuss what our model is as well as the @ModelAttribute annotation firstly though
+let's discuss what the model interfaces, so the model interface defines a holder
+for model attributes and is as such primarily designed for adding attributes
+to the model now that model is exposed o the view which means that we can
+access the attributes in our html
 
 
  */
