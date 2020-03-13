94b31287349408b667bacad4d8fb58d05bde9a84
Ahmed <ahmad.rizgar.rashid@gmail.com>
2020-03-13 23:04:34 +0100

    spring mvc

--- todo-list/src/main/java/academy/learnprogramming/todolist/TodoListApplication.java
+++ todo-list/src/main/java/academy/learnprogramming/todolist/TodoListApplication.java
@@ -5,13 +5,25 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /*
 
-We are going to start by setting up a web application plugins we're going to begin by adding the maven War plugin so what
-actually is the maven war plug-in and actually what is a war file well war which stands for web application archive
-is a file format similar to jar the Java archive file the difference is that war
-files contain the resources necessary for our developing web applications
+As mentioned earlier the spring MVC, Model View controller, is designed around the dispatcher servlet which plays the
+role of a front controller. So in other words it receives all the requests for your application. We can map requests to
+methods in classes annotated with the @controller annotation then these classes
+are known unsurprisingly has annotated controllers or controller classes now
+the spring MVC provides an annotation based programming model where the controller and rest controller
+components use annotations to express things like request mappings request
+import exception handling and more
 
-The WAR Plugin is responsible for collecting all artifact dependencies,
-classes and resources of the web application and packaging them into a web application archive.
+The @RequestMapping annotation is used map requests to controller methods.
+It has various attributes to match by URL, HTTP protocol, request parameters, headers
+and media types what we'll do is use specific shortcut variants of the
+request mapping annotation.
+
+Shortcut variants of @RequestMapping are:
+- @GetMapping(For mapping HTTP get Requests)
+- @PostMapping(For mapping HTTP post Requests)
+- @PutMapping(For mapping HTTP put Requests)
+- @DeleteMapping(For mapping HTTP delete Requests)
+- @PatchMapping(For mapping HTTP patch Requests)
 
 
  */
--- todo-list/src/main/java/academy/learnprogramming/todolist/controllers/IndexController.java
+++ todo-list/src/main/java/academy/learnprogramming/todolist/controllers/IndexController.java
@@ -1,12 +1,28 @@
 package academy.learnprogramming.todolist.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
+/*
+so now that on this line here on line nine now that we've defined that
+when we open the application URL with the slash hello path appended to the
+end the HTTP GET request will be mapped to the method we've just created and
+this method will be invoked.
+ */
 
-    @RequestMapping({"","/","index","index.html"})
+/*
+@ResponseBody indicates that a method returned values should be bound to the web
+response body which in our case means that the bitch which you can see returning on
+will be written to the responsebody which the browser will then display. Other wise trying will think that you are trying to
+return a view.
+ */
+
+    @ResponseBody
+    @GetMapping("/hello")
     public String index(){
         return "index";
     }
